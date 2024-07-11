3595da6d5733fbd38e88eb8034725bac83fb07d8
Yossef Hisham <93604359+yossev@users.noreply.github.com>
2024-07-11 22:28:24 +0300

    Time: 30 ms (54.56%), Space: 12 MB (16.71%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # My First Ever Oneliner.
+        return (Counter(nums1)&Counter(nums2)).elements()
