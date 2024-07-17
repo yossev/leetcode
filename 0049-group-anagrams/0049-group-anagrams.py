8fc5d8560fef6bf60891ff5a2f1162ac3051b411
Yossef Hisham <93604359+yossev@users.noreply.github.com>
2024-07-17 22:15:35 +0300

    Time: 70 ms (69.29%), Space: 15.7 MB (65.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+    # Using hashmaps
+
+        anagrams = {}
+
+    # Sort all the strs
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))     
+            if sorted_word not in anagrams:
+                anagrams[sorted_word] = []
+            anagrams[sorted_word].append(word)
+    
+        return list(anagrams.values())
+            
+
+    
