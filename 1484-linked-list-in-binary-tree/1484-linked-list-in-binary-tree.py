ed50c51b98d66367bef73b4a897a225bf2cfa35d
Yossef Hisham <93604359+yossev@users.noreply.github.com>
2024-09-08 23:57:30 +0300

    Time: 75 ms (49.17%), Space: 14 MB (68.33%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.py
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.py
@@ -12,13 +12,13 @@
 class Solution(object):
     def checkPath(self, head, root):
         if not head:
-            return True
-        
+            return True # We reached the end of the Tree
 
         if not root or head.val != root.val:
             return False
-
+        
         return self.checkPath(head.next, root.left) or self.checkPath(head.next, root.right)
+
 
 
     def isSubPath(self, head, root):
@@ -30,6 +30,7 @@ class Solution(object):
         if not root:
             return False
         
-
         return (head.val == root.val and self.checkPath(head, root)) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)      
+
+
     
