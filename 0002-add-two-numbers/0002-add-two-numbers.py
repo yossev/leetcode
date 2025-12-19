2f36bd85ff783251333d8b9ec73369cc9c6f8ec4
Yossef Hisham <93604359+yossev@users.noreply.github.com>
2025-12-20 01:13:55 +0200

    0002-ADD-TWO-NUMBERS Stats: Time: 7 ms (64.62%), Space: 12.6 MB (14.44%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,32 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        dummy = ListNode()
-        cur = dummy
+        num1 = 0
+        num2 = 0
 
-        carry = 0
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+        temp1, temp2 = l1, l2
 
-            val = v1 + v2 + carry
+        place = 1
+        while temp1:
+            num1 += temp1.val * place
+            place *= 10
+            temp1 = temp1.next
 
-            carry = val // 10
-            val =  val % 10
-            cur.next =  ListNode(val)
+        place = 1
+        while temp2:
+            num2 += temp2.val * place
+            place *= 10
+            temp2 = temp2.next
 
-            # Move ptrs
-            cur = cur.next
-            l1 =  l1.next if l1 else None
-            l2 = l2.next if l2 else None
+        _sum = num1 + num2
+
+        dummy = ListNode(-1)
+        curr = dummy
+
+        for digit in reversed(str(_sum)):
+            curr.next = ListNode(int(digit))
+            curr = curr.next
 
         return dummy.next
