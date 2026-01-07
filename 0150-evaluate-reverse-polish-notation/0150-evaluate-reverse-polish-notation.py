3fc45e8cc65d1ca36521c63354c451405e2759ef
Yossef Hisham <93604359+yossev@users.noreply.github.com>
2026-01-07 15:55:48 +0200

    0150-EVALUATE-REVERSE-POLISH-NOTATION Stats: Time: 9 ms (54.97%), Space: 13.9 MB (9.16%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,33 +1,33 @@
 class Solution(object):
     def isOperator(self, s):
         return s in {'+', '-', '*', '/'}
-        
 
+    
     def evaluate(self, a, b, op):
-        if op == "+":
+        if op == '+':
             return b + a
-        elif op == "-":
+        elif op == '-':
             return b - a
-        elif op == "*":
+        elif op == '*':
             return b * a
-        elif op == "/":
+        elif op == '/':
             return int(float(b) / a)
         else:
             return 0
-    
-    
+
+
     def evalRPN(self, tokens):
         """
         :type tokens: List[str]
         :rtype: int
         """
         stack = []
+
         for token in tokens:
-            if(self.isOperator(token)):
-               a = stack.pop()
-               b = stack.pop()
-               stack.append(self.evaluate(a, b, token))
+            if (self.isOperator(token)):
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(self.evaluate(a, b, token))
             else:
                 stack.append(int(token))
-
         return stack.pop()
